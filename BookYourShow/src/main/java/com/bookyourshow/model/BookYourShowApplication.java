f637c44284a2e768e8e13c63405b601064c1f5d8
Palthady Monappa <p.palthady.monappa@accenture.com>
2022-06-01 17:13:18 +0530

    initial commit

---
+++ BookYourShow/src/main/java/com/bookyourshow/model/BookYourShowApplication.java
@@ -0,0 +1,13 @@
+package com.bookyourshow.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourShowApplication.class, args);
+	}
+
+}
---
+++ BookYourShow/src/test/java/com/bookyourshow/model/BookYourShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookyourshow.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
